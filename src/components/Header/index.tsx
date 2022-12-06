091e92e9aef1610e9474e0f1f2bab4dc44878f5e
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-12-05 22:44:54 -0300

    fix: logo relative path

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ interface HeaderProps {}
 export function Header({}: HeaderProps) {
   return (
     <header className={styles.header}>
-      <img src="/src/assets/logo.png" alt="To-do List Logo" />
+      <img src="../../../src/assets/images/logo.png" alt="To-do List Logo" />
     </header>
   )
 }
